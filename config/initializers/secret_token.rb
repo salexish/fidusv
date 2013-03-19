e26206ca98d0edf551875fc2ac7fc00c2dffc6a7
Sarah Hahn <sahahn@outlook.com>
2013-03-19 19:09:19 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fidusv::Application.config.secret_token = '187aae87b71903621052b406c2dc27de240dccf9214850a13e79c9ebf533b6500a5cc5031b9e65e7e2a7efb7a1f08a31a30b87ed5fae16a61d2a2685838086c6'
